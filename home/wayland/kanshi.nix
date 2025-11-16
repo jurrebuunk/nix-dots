0c55512d1173b1b7fa28fc3d732f2caa7a1736ac
Jurre Buunk <jurre@buunk.org>
2025-11-16 20:16:39 +0100

    sdf

--- home/wayland/kanshi.nix
+++ home/wayland/kanshi.nix
@@ -1,9 +1,7 @@
-# home/kanshi.nix
 { config, pkgs, ... }:
 
 let
   kanshiConfig = ''
-    # Alleen externe LG ultrawide gebruiken
     profile lg-ultrawide-only {
       output eDP-1 disable
       output DP-4 enable mode 2560x1080 position 0,0
@@ -16,9 +14,12 @@ in
   xdg.configFile."kanshi/config".text = kanshiConfig;
 
   systemd.user.services.kanshi = {
-    enable = true;
     description = "Kanshi display manager";
-    serviceConfig.ExecStart = "${pkgs.kanshi}/bin/kanshi";
-    serviceConfig.Restart = "on-failure";
+    after = [ "graphical.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.kanshi}/bin/kanshi";
+      Restart = "on-failure";
+    };
+    wantedBy = [ "default.target" ];
   };
 }
